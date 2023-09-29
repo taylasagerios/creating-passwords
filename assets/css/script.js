cd9731dbd60963d6bf129d1b91b5c7bb0789d5bf
Tayla Rios <145400166+taylasagerios@users.noreply.github.com>
2023-09-28 17:54:55 -0700

    created a function to write the password and added two variables and defined the value for them

--- assets/css/script.js
+++ assets/css/script.js
@@ -41,3 +41,11 @@ function generatePassword(){
 }
 
 var generateBtn = document.querySelector("#generate");
+
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  
+  }
