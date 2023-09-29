87d1e5e680490512fc28d4822f26e22b24ed8a01
Tayla Rios <145400166+taylasagerios@users.noreply.github.com>
2023-09-28 17:43:29 -0700

    put in prompt and confirm formats and concol logged them

--- assets/css/script.js
+++ assets/css/script.js
@@ -1,3 +1,14 @@
 function generatePassword(){
     console.log("generatepassword")   
+
+    let passwordLength= prompt("enter number between 8 and 128");
+    console.log (passwordLength)
+    let lowercase= confirm("do you want to add a lowercase letter");
+    console.log (lowercase)
+    let uppercase= confirm("do you want to add a uppercase letter");
+    console.log (uppercase)
+    let numeric= confirm("do you want to add a number");
+    console.log (numeric)
+    let specialcharacters= confirm("do you want to add a special character");
+    console.log (specialcharacters)  
 }
